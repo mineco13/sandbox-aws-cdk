e93425df8b462b263facf75b49d5a2375f992095
mineharuakimasa <minehelloworld1@gmail.com>
2021-04-06 00:08:26 +0900

    add vpc and subnet

--- lib/sandbox_cdk-stack.ts
+++ lib/sandbox_cdk-stack.ts
@@ -1,10 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnEgressOnlyInternetGateway, CfnInternetGateway, IVpc, RouterType, Subnet, SubnetConfiguration, SubnetSelection, SubnetType, Vpc } from 'aws-cdk-lib/lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class SandboxCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const vpc = new Vpc(this, "vpc", {
+      cidr: "10.0.0.0/16",
+      natGateways: 0,
+      subnetConfiguration: [SubnetType.ISOLATED, SubnetType.PUBLIC].map(type => this.subnetConfig(type))
+    })
 
-    // The code that defines your stack goes here
+  }
+
+  subnetConfig(type: SubnetType) {
+    return {
+      cidrMask: 18, subnetType: type, name: type
+    }
   }
 }
