3dcf3311ecc5dd2565ee9501c9c7b0bcc2c07e1b
mineharuakimasa <minehelloworld1@gmail.com>
2021-04-07 00:44:57 +0900

    add security group from internet

--- lib/resource/security/security_group.ts
+++ lib/resource/security/security_group.ts
@@ -1,12 +1,17 @@
-import { IVpc, Port, SecurityGroup, SecurityGroupProps } from "aws-cdk-lib/lib/aws-ec2"
+import { IVpc, Peer, Port, SecurityGroup, SecurityGroupProps } from "aws-cdk-lib/lib/aws-ec2"
 
 
 export function sgInternal(securityGroup: (props: SecurityGroupProps) => SecurityGroup, vpc: IVpc) {
-    const internal = securityGroup({ securityGroupName: "internal", vpc: vpc })
+    const internal = securityGroup({ securityGroupName: "internal", vpc: vpc, allowAllOutbound: false })
     internal.addIngressRule(internal, Port.allTraffic())
     internal.addEgressRule(internal, Port.allTraffic())
     return internal
 }
 export function sgToInternet(vpc: IVpc) {
     return { securityGroupName: "to-internet", vpc: vpc, allowAllOutbound: true }
+}
+export function sgFromInternet(securityGroup: (props: SecurityGroupProps) => SecurityGroup, vpc: IVpc) {
+    const fromInternet = securityGroup({ securityGroupName: "from-internet", vpc: vpc, allowAllOutbound: false })
+    fromInternet.addIngressRule(Peer.anyIpv4(), Port.tcp(443))
+    return fromInternet
 }
--- lib/stack/security.ts
+++ lib/stack/security.ts
@@ -11,12 +11,14 @@ export class SecurityStack extends Stack {
   readonly securityGroup: {
     readonly internal: SecurityGroup,
     readonly toInternet: SecurityGroup,
+    readonly fromInternet: SecurityGroup,
   }
   constructor(scope: Construct, id: string, props: SecurityProps) {
     super(scope, id, props);
     this.securityGroup = {
       internal: sgInternal((props: SecurityGroupProps) => new SecurityGroup(this, "internal", props), props.vpc),
       toInternet: new SecurityGroup(this, "to-internet", sgToInternet(props.vpc)),
+      fromInternet: sgFromInternet((props: SecurityGroupProps) => new SecurityGroup(this, "from-internet", props), props.vpc),
     }
   }
 }
