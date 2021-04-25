e21e297d9ac2e6c3fa7f9d90e234d714a57d81d9
mineharuakimasa <minehelloworld1@gmail.com>
2021-04-25 20:43:55 +0900

    change instance timer

--- lib/resource/service/rule.ts
+++ lib/resource/service/rule.ts
@@ -1,5 +1,5 @@
 import { RuleProps, Schedule } from "aws-cdk-lib/lib/aws-events";
 
 export function eodRule(): RuleProps {
-    return { schedule: Schedule.cron({ hour: "17" }) }
+  return { schedule: Schedule.cron({ hour: "20", minute: "0" }) };
 }
